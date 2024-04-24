57e524ad3b9f77429f68fb66bd9bc7fd1a7071fc
fdjerrar <farid.djerrar@soprasteria.com>
2024-04-24 12:02:02 +0200

    mis en, forme

---
+++ Mov-VueTw/src/components/header/navBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex">
+        <router-link to="/" ><img src="../../assets/ds.jpg" alt="DS Logo" class="size-3/12 opacity-65 h-16 w-auto">
+        </router-link>
+        <ul class="flex mt-7">
+            <li><router-link to="/" class="ml-5  text-2xl" >Home</router-link></li>
+            <li><router-link to="/films" class="ml-6 text-2xl" >Films</router-link></li>
+            <li><router-link  to="/series" class="ml-6 text-2xl">Séries</router-link></li>
+            <li><router-link to="/animations" class="ml-6 text-2xl" >Animations</router-link></li>
+        </ul>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
