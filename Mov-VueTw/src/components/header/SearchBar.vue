ebd1cdbb4c636c7945e0e66d09432eb001b3cd09
fdjerrar <farid.djerrar@soprasteria.com>
2024-04-25 12:03:17 +0200

    SearchBar révisée

--- Mov-VueTw/src/components/header/SearchBar.vue
+++ Mov-VueTw/src/components/header/SearchBar.vue
@@ -1,8 +1,17 @@
 <template>
-    <div class="mt-3 flex" >
-        <input type="text" placeholder="Rechercher un Film, un acteur ou une série..." class="rounded-full bg-green-800 px-auto h-2/4 w-72 focus:shadow mt-3">
+    <div class="mt-3 size-fit  flex relative" >
+        <input 
+            type="text" 
+            placeholder="Rechercher un Film, un acteur,une série..." class="rounded-full bg-green-800 focus:shadow w-96 pl-7  "
+            >
+            <div class="absolute top-5 ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+            </div>
 
-        <img src="../../assets/Goku-Dragon.webp" alt="avatar de profil" class="size-16 rounded-full mr-4 ml-7">
+
+        <img src="../../assets/Goku-Dragon.webp" alt="avatar de profil" class="size-16 rounded-full mr-1 ml-12">
     </div>
 </template>
 
