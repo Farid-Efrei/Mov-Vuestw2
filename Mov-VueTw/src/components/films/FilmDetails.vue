01b4749b11040d09fa945dba4d205f935f3e1845
fdjerrar <farid.djerrar@soprasteria.com>
2024-05-03 20:56:17 +0200

    ajout icone dans fiche détaillée film/serie

--- Mov-VueTw/src/components/films/FilmDetails.vue
+++ Mov-VueTw/src/components/films/FilmDetails.vue
@@ -4,62 +4,69 @@
             <img :src="imageUrl" alt="Joker" class="w-[100%] max-w-[100]" />
 
             <div class="ml-24  ">
-                <h1 class="text-5xl font-semibold">{{ video.title || video.name }}</h1>
+                <div class="flex justify-between items-center">
+                    <h1 class="text-5xl font-semibold">{{ video.title || video.name }}</h1>
+                    <img src="../../assets/filmIcone.webp" alt="icone-film" class="size-20" v-if="magicRoute ===false">
+                    <img src="../../assets/tvseries.webp" alt="icone-serie" class="size-20" v-if="magicRoute" >
+                </div>
+
                 <span class="text-green-200 text-xl flex mt-4 border flex-col pl-2">
-                   
-                    <div class="flex"> 
+
+                    <div class="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                        class="w-8 mr-3 text-yellow-400">
-                        <path fill-rule="evenodd"
-                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                            clip-rule="evenodd" />
-                    </svg>
-                        {{ (video.vote_average * 10).toFixed(2) }}%  |  Date de sortie: {{ video.release_date  || video.first_air_date }}  |
-                    </div> 
+                            class="w-8 mr-3 text-yellow-400">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        {{ (video.vote_average * 10).toFixed(2) }}% | Date de sortie: {{ video.release_date ||
+                video.first_air_date }} |
+                    </div>
                     <div class="flex-inline">
                         <span v-if="video && video.last_episode_to_air">
                             <u>Durée approximative :</u>
-                       {{ video.last_episode_to_air.runtime }}
-                       min 
-                       </span>
-                       <span v-if="video && video.runtime">
-                           <u>Durée approximative :</u>
-                           {{ video.runtime }}min
-                       </span>
-                       <div class="">
-                        Type : Film ou Série |  {{ $route.params.magicRoute }}<br>
-                        
-                        <div v-if="magicRoute && video && video.last_episode_to_air && video.networks" >
-                       
-                        Nombre de Saison(s): 12 | {{ video.number_of_seasons }}<br> 
-                        Nombre d'épisodes (prévus):24 |  {{ video.number_of_episodes }}<br>
-                        Origine: {{ video.origin_country }}<br>
-                        Dernier épisode diffusé: numéro épisode et titre : 7 le réveil de Dai | {{ video.last_episode_to_air.episode_number }} - {{ video.last_episode_to_air.name }}<br>
-                        Statut de la série : en cours | {{ video.status }} <br>
-                        Produit par : {{ video.networks[0].name }}  <br>
+                            {{ video.last_episode_to_air.runtime }}
+                            min
+                        </span>
+                        <span v-if="video && video.runtime">
+                            <u>Durée approximative :</u>
+                            {{ video.runtime }}min
+                        </span>
+                        <div class="">
+                            Type : Film ou Série | {{ $route.params.magicRoute }}<br>
+
+                            <div v-if="magicRoute && video && video.last_episode_to_air && video.networks">
+
+                                Nombre de Saison(s): 12 | {{ video.number_of_seasons }}<br>
+                                Nombre d'épisodes (prévus):24 | {{ video.number_of_episodes }}<br>
+                                Origine: {{ video.origin_country }}<br>
+                                Dernier épisode diffusé: numéro épisode et titre : 7 le réveil de Dai | {{
+                video.last_episode_to_air.episode_number }} - {{ video.last_episode_to_air.name }}<br>
+                                Statut de la série : en cours | {{ video.status }} <br>
+                                Produit par : {{ video.networks[0].name }} <br>
+                            </div>
+
+
                         </div>
-
-
-                       </div>
 
                     </div>
                     <div>
-                     <br> <b><u>Genres:</u></b>
-                     <p v-for="(genre, index) in video.genres" :key="index" class=" ">
-                        {{ '| ' + genre.name }}
-                        <span v-if="index !== video.genres.length - 1">, </span>
-                    </p>
+                        <br> <b><u>Genres:</u></b>
+                        <p v-for="(genre, index) in video.genres" :key="index" class=" ">
+                            {{ '| ' + genre.name }}
+                            <span v-if="index !== video.genres.length - 1">, </span>
+                        </p>
                     </div>
-                    
-                    
+
+
                 </span>
                 <p class="mt-7 text-justify shadow-2xl">
                     {{ video.overview }}
                     <br /><br />
 
-                   <span class="font-semibold text-red-300">
-                     "{{ video.tagline }}"
-                   </span>
+                    <span class="font-semibold text-red-300">
+                        "{{ video.tagline }}"
+                    </span>
                     <!-- En 1981, Arthur Fleck travaille dans une agence de clowns à Gotham City. Méprisé et incompris par ceux qui lui font face, il mène une morne vie en marge de la société et habite dans un immeuble miteux avec sa mère Penny. Un soir, il se fait agresser dans le métro par trois traders de Wayne Enterprises alcoolisés qui le brutalisent, le poussant à les tuer en retour. Son geste inspire à une partie de la population l'idée de s'en prendre eux aussi aux puissants. Dans cette société décadente, Arthur bascule peu à peu dans la folie et finit par devenir le Joker, un dangereux tueur psychopathe victime d'hallucinations et le plus grand criminel de Gotham City. -->
                 </p>
                 <div class="mt-10">
@@ -69,12 +76,12 @@
                     <div class="flex gap-10">
                         <div class="flex flex-col">
                             <span v-if="video && video.created_by">
-                                Todd Philips |  {{ video.created_by[0].name }}</span>
+                                Todd Philips | {{ video.created_by[0].name }}</span>
                             <span class="text-green-300">Realisateur</span>
                         </div>
 
                         <div class="flex flex-col">
-                            <span v-if="video && video.created_by" > Scott Silver | {{ video.created_by[1].name }}</span>
+                            <span v-if="video && video.created_by"> Scott Silver | {{ video.created_by[1].name }}</span>
                             <span class="text-green-300">Producteur</span>
                         </div>
                     </div>
@@ -118,7 +125,7 @@ export default {
     data() {
         return {
             video: {},
-            last_episode_to_air : '',
+            last_episode_to_air: '',
         }
     },
     mounted() {
@@ -135,7 +142,7 @@ export default {
 
              this.fetchFilm(this.$route.params.id)
          } */
-         
+
     },
     methods: {
 
@@ -153,41 +160,41 @@ export default {
              )
              this.video = response.data
          }, */
-         async fetchSerieOrFilm(videoId) {
-             try {
-                 const magicRoute = this.$route.params.magicRoute;
-                 if (magicRoute === 'films') {
-                         const response = await requete(
-                             `/movie/${videoId}`
-                            )
-                            this.video = response.data
-                        } else {
-                            const response = await requete(
-                                '/tv/' + videoId
-                            )
-                            this.video = response.data
-                        } 
-                } catch (error) {
-                    console.error("Une erreur s'est produite lros de la récupération de la video", error)
+        async fetchSerieOrFilm(videoId) {
+            try {
+                const magicRoute = this.$route.params.magicRoute;
+                if (magicRoute === 'films') {
+                    const response = await requete(
+                        `/movie/${videoId}`
+                    )
+                    this.video = response.data
+                } else {
+                    const response = await requete(
+                        '/tv/' + videoId
+                    )
+                    this.video = response.data
                 }
-            },
+            } catch (error) {
+                console.error("Une erreur s'est produite lros de la récupération de la video", error)
+            }
         },
+    },
     computed: {
         imageUrl() {
-            return 'https://image.tmdb.org/t/p/original/'+ this.video.poster_path
+            return 'https://image.tmdb.org/t/p/original/' + this.video.poster_path
         },
-        
+
         magicRoute() {
             const magicRoute = this.$route.params.magicRoute;
-            if (magicRoute ==='series'){
+            if (magicRoute === 'series') {
                 return true;
             } else {
                 return false;
             }
 
-            
+
         }
-        
+
     },
 }
 </script>
