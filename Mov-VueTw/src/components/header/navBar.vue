9db950cd915facc4272d13f72ccb95f61f9d5166
Jane Doe <janedoe@exemple.com>
2024-05-17 01:35:05 +0200

    ok

--- Mov-VueTw/src/components/header/navBar.vue
+++ Mov-VueTw/src/components/header/navBar.vue
@@ -2,11 +2,11 @@
     <div class="flex">
         <router-link to="/" ><img src="../../assets/ds.jpg" alt="DS Logo" class="size-3/12 opacity-65 h-16 w-auto">
         </router-link>
-        <ul class="flex mt-7 ">
-            <li><router-link to="/" class="ml-5  text-3xl hover:bg-yellow-300 rounded-xl px-7 py-5  hover:text-green-900 hover:text-4xl " >Home</router-link></li>
-            <li><router-link to="/films" class="ml-6 text-3xl hover:bg-yellow-300 rounded-xl px-7 py-6  hover:text-green-900 hover:text-4xl " >Films</router-link></li>
-            <li><router-link  to="/series" class="ml-6 text-3xl hover:bg-yellow-300 rounded-xl px-7 py-6  hover:text-green-900 hover:text-4xl ">Séries</router-link></li>
-            <li><router-link to="/animations" class="ml-6 text-3xl hover:bg-yellow-300 rounded-xl px-7 py-6  hover:text-green-900 hover:text-4xl hover:border-b-8 border-sky-300" >Animations</router-link></li>
+        <ul class="flex mt-5">
+            <li><router-link to="/" class="ml-3 " >Home</router-link></li>
+            <li><router-link to="/films" class="ml-4 " >Films</router-link></li>
+            <li><router-link  to="/series" class="ml-4 ">Séries</router-link></li>
+            <li><router-link to="/animations" class="ml-4 " >Animations</router-link></li>
         </ul>
 
     </div>
