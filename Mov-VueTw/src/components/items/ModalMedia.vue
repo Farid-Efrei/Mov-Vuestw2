bd6dd22ef07d16ef4027f745c34f6d8f13ec343f
Jane Doe <janedoe@exemple.com>
2024-05-18 23:13:09 +0200

    Modal YT OK

--- Mov-VueTw/src/components/items/ModalMedia.vue
+++ Mov-VueTw/src/components/items/ModalMedia.vue
@@ -1,9 +1,9 @@
 <template>
-    <div v-if="showModal" class="fixed  inset-0 z-50 flex items-center justify-center bg-opacity-75">
-        <div class="relative bg-green-100 rounded-lg  p-4 max-w-full max-h-full   overflow-hidden">
+    <div v-if="showModal" class="fixed  inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 backdrop-blur-sm">
+        <div class="relative bg-green-100 rounded-lg  p-4  w-full h-full max-w-7xl max-h-full   overflow-hidden">
             <button 
             @click="closeModal"
-            class="absolute top-0 right-0 m-2 text-yellow-300"
+            class="absolute top-0 right-0 m-2 text-yellow-100 p-1"
             >
             X Fermer
             </button>
@@ -15,14 +15,14 @@
                 class="max-w-full max-h-full"
                 >
             </div>
-            <div v-else-if="mediaType === 'video'" class="aspect-w-16 aspect-h-9">
+            <div v-else-if="mediaType === 'video'" class="aspect-w-16 aspect-h-9  w-full h-full">
 
                 <iframe 
                 :src="mediaSrc" 
                 frameborder="0"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
-                class="w-full h-full "
+                class="w-full h-full"
                 >
             </iframe>
         </div>
