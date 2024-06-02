1eed93b70a272badd7951c717e1035798a12d854
Farid-Efrei <128361230+Farid-Efrei@users.noreply.github.com>
2024-06-02 23:09:44 +0200

    NAvBar toggle + webresp

---
+++ Mov-VueTw/src/components/footer/FooterCompo.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer class="bg-gray-800 text-white p-4">
+    <div class="container mx-auto flex flex-wrap items-center justify-between">
+      <!-- Logo and Text -->
+      <div class="flex items-center space-x-4">
+        <img src="@/assets/Capture.png" alt="Logo" class="h-24 w-auto" />
+        <span>© 2024 Popcorn's, le site dédié au cinéma Populaire. All rights reserved.</span>
+      </div>
+
+      <!-- Contact Link -->
+      <div>
+        <a href="/contact" class="hover:text-gray-300">Contact</a>
+      </div>
+
+      <!-- Social Media Icons -->
+      <div class="flex space-x-4">
+        <a href="https://facebook.fr" target="_blank" class="hover:text-gray-300">
+          <img src="@/assets/facebook.png" alt="Facebook" class="h-6 w-6" />
+        </a>
+        <a href="https://twitter.com" target="_blank" class="hover:text-gray-300">
+          <img src="@/assets/twitter.png" alt="Twitter" class="h-6 w-6" />
+        </a>
+        <a href="https://instagram.com" target="_blank" class="hover:text-gray-300">
+          <img src="@/assets/insta.jpg" alt="Instagram" class="h-6 w-6" />
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterCompo'
+}
+</script>
+
+<style scoped>
+/* Tu peux ajouter des styles spécifiques ici si nécessaire */
+</style>
--- Mov-VueTw/src/components/header/navBar.vue
+++ Mov-VueTw/src/components/header/navBar.vue
@@ -1,57 +1,98 @@
 <template>
-    <div class="container flex ">
-        <img src="../../assets/ds.jpg" alt="DS Logo" class=" opacity-65 h-24 w-auto" @click="handleClick" @dblclick="handleDoubleClick" >
-        
-        <ul class="flex mt-7 ">
-            <li><router-link to="/" class="ml-5  text-3xl hover:bg-yellow-300 rounded-xl px-7 py-5  hover:text-green-900 hover:text-4xl " >Home</router-link></li>
-            <li><router-link :to="{ name: 'films', params : {magicRoute : 'films'}}" class="ml-6 text-3xl hover:bg-yellow-300 rounded-xl px-7 py-6  hover:text-green-900 hover:text-4xl " >Films</router-link></li>
-            <li><router-link  to="/series" class="ml-6 text-3xl hover:bg-yellow-300 rounded-xl px-7 py-6  hover:text-green-900 hover:text-4xl ">Séries</router-link></li>
-            <li><router-link to="/animations" class="ml-6 text-3xl hover:bg-yellow-300 rounded-xl px-7 py-6  hover:text-green-900 hover:text-4xl hover:border-b-8 border-sky-300" >Animations</router-link></li>
-        </ul>
-
+  <div class="container flex">
+    <div class="flex justify-between items-center py-auto">
+      <div>
+        <img
+          src="../../assets/ds.jpg"
+          alt="DS Logo"
+          class="opacity-65 h-24 w-auto"
+          @click="handleClick"
+          @dblclick="handleDoubleClick"
+        />
+        <button @click="toggleMenu" class="ml-6 md:hidden">
+          <svg class="h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-16 6h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <ul
+        :class="{ hidden: !menuOpen, flex: menuOpen }"
+        class="md:flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 md:mt-0"
+      >
+        <li>
+          <router-link
+            to="/"
+            class="ml-6 md:text-3xl text-xl md:hover:bg-yellow-300 rounded-xl px-7 py-5 hover:text-green-900 md:hover:text-4xl hover:text-2xl"
+            >Home</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            :to="{ name: 'films', params: { magicRoute: 'films' } }"
+            class="ml-6 md:text-3xl text-xl md:hover:bg-yellow-300 rounded-xl px-7 py-5 hover:text-green-900 md:hover:text-4xl hover:text-2xl"
+            >Films</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            to="/series"
+            class="ml-6 md:text-3xl text-xl md:hover:bg-yellow-300 rounded-xl px-7 py-5 hover:text-green-900 md:hover:text-4xl hover:text-2xl md:hover:rounded-lg"
+            >Séries</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            to="/animations"
+            class="ml-6 md:text-3xl text-xl md:hover:bg-yellow-300 rounded-xl px-7 py-5 hover:text-green-900 md:hover:text-4xl hover:text-2xl"
+            >Animations</router-link
+          >
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-
-export default{
-    data() {
-        return {
-            clickTimeout:null,
-            films:[],
-        }
-    },
-
-
-methods: {
-    handleClick(){
-        // Annuler le délai si déjà défini
-        if (this.clickTimeout){
-            clearTimeout(this.clickTimeout);
-            this.clickTimeout = null;
-        }
-        // Définir un nouveau délai :
-    this.clickTimeout = setTimeout(() => {
-        this.$router.push('/');
-        this.clickTimeout = null;
-    },300); // délai inférieur à celui du double clic typique.
-    },
-
-    handleDoubleClick(){
-        clearTimeout(this.clickTimeout);
-        this.clickTimeout = null;
-        console.log("KORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR");
-        this.$router.push('/k-films')
+export default {
+  data() {
+    return {
+      clickTimeout: null,
+      films: [],
+      menuOpen: false // Contrôle l'état du menu burger.
     }
-    
-    
-},
+  },
 
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen
+      console.log('menu ouvert')
+    },
+    handleClick() {
+      // Annuler le délai si déjà défini
+      if (this.clickTimeout) {
+        clearTimeout(this.clickTimeout)
+        this.clickTimeout = null
+      }
+      // Définir un nouveau délai :
+      this.clickTimeout = setTimeout(() => {
+        this.$router.push('/')
+        this.clickTimeout = null
+      }, 300) // délai inférieur à celui du double clic typique.
+    },
+
+    handleDoubleClick() {
+      clearTimeout(this.clickTimeout)
+      this.clickTimeout = null
+      console.log('KORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR')
+      this.$router.push('/k-films')
+    }
+  }
 }
-
 </script>
 
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
